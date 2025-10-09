b87d438943044782a31b9021b3037e37b90bd4a4
Vaughner <bongerolaga@example.com>
2025-10-09 10:40:38 +0800

    AGAYAGAYAGAYAGAYAGAY

--- app/src/main/java/com/capstone/healthradar/ApiClient.kt
+++ app/src/main/java/com/capstone/healthradar/ApiClient.kt
@@ -4,7 +4,8 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
-    private const val BASE_URL = "https://your-project.vercel.app/api" // ⚡ change to your FastAPI IP
+    // ✅ remove "/api/"
+    private const val BASE_URL = "http://192.168.1.9:8000/"
 
     val retrofit: Retrofit by lazy {
         Retrofit.Builder()
